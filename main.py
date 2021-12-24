420e76938c14323fa5662721a34081d56f09ac48
tyrenG123 <96602498+tyeedle@users.noreply.github.com>
2021-12-24 18:49:41 +0000

    .

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ for i in range(len(a)):
 
     dimensions = [f"{len(a)}x{len(a[i])}"]
 
-print(dimensions)
+a2 = np.zeros(len(a))
+print(a2)
+
+b1 = np.array([1,2,3,4,5])
+b2 = np.array([6,7,8,9,10])
+
+b3 = np.concatenate((b1,b2))
+
+def return_function(list_,excludeindex):
+    s_ = sum(list_) - list_[excludeindex]
+    return s_
+    
+returned_ = return_function(b3,1)
+print(b3[1] + returned_)
