2fc3a8ef8b131e303a7d1cbf89f0e8b3a4c64489
tyrenG123 <96602498+tyeedle@users.noreply.github.com>
2021-12-25 00:11:56 +0000

    .

---
+++ file2.py
@@ -0,0 +1,13 @@
+import time
+import random
+array_ = ['log1','log2','error1']
+txt_ = 'newfile.txt'
+
+def writeToTxtFile(txt_file,text):
+    with open(txt_file,'w') as f:
+        f.write(f'{text}\n')
+
+while True:
+    writeToTxtFile(txt_,random.choice(array_))
+    time.sleep(0.1)
+
